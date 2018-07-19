8ec6a5f8015f8496a330bba571af3db952198e28
Hunter Pfeiffer <whpfeiff@umich.edu>
2018-07-18 22:44:06 -0700

    MOD: install script for ubuntu 2018

--- install/installer.sh
+++ install/installer.sh
@@ -3,6 +3,7 @@
 sudo apt-get update
 sudo apt-get upgrade
 sudo apt-get dist-upgrade
+
 #most used programs 
 sudo apt-get install vim-gnome -y
 sudo apt-get install tmux -y
@@ -11,34 +12,50 @@ sudo apt-get install htop -y
 sudo apt-get install xournal -y
 sudo apt-get install python3-pip -y #python
 sudo pip3 install --upgrade pip
+sudo snap install gitkraken
+sudo snap install discord
+sudo snap install zenkit
+
+#NOTE use airline plugin instead
 #powerline
-sudo pip3 install git+git://github.com/Lokaltog/powerline
-sudo wget https://github.com/powerline/powerline/raw/develop/font/PowerlineSymbols.otf
-sudo wget https://github.com/powerline/powerline/raw/develop/font/10-powerline-symbols.conf
-sudo mv PowerlineSymbols.otf /usr/share/fonts/
-sudo fc-cache -vf /usr/share/fonts/
-sudo mv 10-powerline-symbols.conf /etc/fonts/conf.d/
+#sudo apt-get install python3-testresources -y
+##sudo pip3 install git+git://github.com/Lokaltog/powerline
+#pip install --user git+git://github.com/Lokaltog/powerline
+#sudo wget https://github.com/powerline/powerline/raw/develop/font/PowerlineSymbols.otf
+#sudo wget https://github.com/powerline/powerline/raw/develop/font/10-powerline-symbols.conf
+#sudo mv PowerlineSymbols.otf /usr/share/fonts/
+#sudo fc-cache -vf /usr/share/fonts/
+#sudo mv 10-powerline-symbols.conf /etc/fonts/conf.d/
+
 # mpsyoutube
 sudo pip3 install mps-youtube
 sudo pip3 install youtube-dl
 sudo apt-get install mpv -y 
+
 # YCM
 sudo apt-get install build-essential cmake -y
 sudo apt-get install python-dev python3-dev -y
-# part of color coded, need to the lua part
-sudo apt-get install build-essential libclang-3.9-dev libncurses-dev libz-dev cmake xz-utils libpthread-workqueue-dev -y
-sudo add-apt-repository ppa:ubuntu-toolchain-r/test
-sudo apt-get update
-sudo apt-get install g++-4.9 -y
-sudo update-alternatives --remove-all g++
-sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-4.9 50
+sudo apt-get install npm -y #optional for js and ts
+
 #other
-sudo apt-get install gsmartcontrol -y #for inspecting SSD helth
-sudo apt-get install ibus-anthy -y #for japanese
+#sudo apt-get install gsmartcontrol -y #for inspecting SSD helth
+#sudo apt-get install ibus-anthy -y #for japanese
+
 #apt-get install compizconfig-settings-manager compiz-plugins-extra -y
-sudo apt-get install gnome-tweak-tool -y
-sudo apt-get install steam -y
+#sudo apt-get install gnome-tweak-tool -y
+#sudo apt-get install steam -y
+
+# color coded, assumes you need lua 5.5
+# sudo apt-get install build-essential libclang-3.9-dev libncurses-dev libz-dev cmake xz-utils libpthread-workqueue-dev -y
+# sudo add-apt-repository ppa:ubuntu-toolchain-r/test
+# sudo apt-get update
+# sudo apt-get install g++-4.9 -y
+# sudo update-alternatives --remove-all g++
+# sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-4.9 50 -y
+# sudo add-apt-repository ppa:ubuntu-toolchain-r/test
+# sudo apt-get update
+# sudo apt-get install g++-4.9
+# sudo apt-get install liblua5.5-dev lua5.5
+
 #clean up
 sudo apt-get autoremove -y
-#prep for next step in README
-vim --version | grep lua
--- install/installer_vimPluggins.sh
+++ install/installer_vimPluggins.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 #ycm
 cd ~/.vim/bundle/YouCompleteMe
-./install.py --clang-completer --js-completer
+./install.py --clang-completer
 
 #color Coded
-cd ~/.vim/bundle/color_coded
-mkdir build && cd build
-cmake ..
-make && make install # Compiling with GCC is preferred, ironically
-# Clang works on OS X, but has mixed success on Linux and the BSDs
+# cd ~/.vim/bundle/color_coded
+# mkdir build && cd build
+# cmake ..
+# make && make install # Compiling with GCC is preferred, ironically
+# # Clang works on OS X, but has mixed success on Linux and the BSDs
 
-# Cleanup afterward; frees several hundred megabytes
-make clean && make clean_clang
+# # Cleanup afterward; frees several hundred megabytes
+# make clean && make clean_clang
 #etc
